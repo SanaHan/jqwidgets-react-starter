43adf62401565224e2ed11576b9d4b0974b266a2
brunesto <brunesto@github.com>
2017-01-20 00:20:47 +0100

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -14,12 +14,13 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
+        // explicitely define the path to allow import of the jqwidget-react jsx files
         include: [
-        path.resolve(__dirname, "App.js"),
-        path.resolve(__dirname, "main.js"),
-         path.resolve(__dirname, "node_modules/jqwidgets-framework/jqwidgets-react")
-      ],
-        aaaexclude: /node_modules/,
+          path.resolve(__dirname, "src"),
+          path.resolve(__dirname, "main.js"),
+          path.resolve(__dirname, "node_modules/jqwidgets-framework/jqwidgets-react")
+        ],
+        // exclude: /node_modules/,
         loader: 'babel',
         query: {
           presets: ['es2015', 'react']
